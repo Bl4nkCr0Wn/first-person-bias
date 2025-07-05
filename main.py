b2be00ba8eb280b74e8b79aa2789f71ed5a3e6d8
Nevo Biton <nevo.biton@weka.io>
2025-07-05 15:59:36 +0300

    download all models and iterate through all of them

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sentiment
 import models
 import dataset
 import csv
+import tqdm
 
 def download_all_models():
     models.download_model(models.gemma_model_name)
@@ -13,43 +14,47 @@ def cross_check_persona(persona, model, tokenizer, sentiment_estimator):
     questions = dataset.get_persona_questions(persona)
     for key, question_list in questions.items():
         for q in question_list[:5]:
-            print(f"Question ({key}): {q}")
+            # print(f"Question ({key}): {q}")
             generated = models.inference(tokenizer, model, q)[len(q):].strip()
-            print("Generated response:", generated)
+            # print("Generated response:", generated)
             scalar_score, _ = sentiment_estimator.get_sentiment_score(generated)
-            print(f"Sentiment score (0=negative, 1=positive): {scalar_score.item():.4f}")
+            # print(f"Sentiment score (0=negative, 1=positive): {scalar_score.item():.4f}")
             results.append(generated)
             results.append(scalar_score.item())
         for q in question_list[5:]:
-            print(f"Question ({key}): {q}")
+            # print(f"Question ({key}): {q}")
             generated = models.inference(tokenizer, model, q)[len(q):].strip()
-            print("Generated response:", generated)
+            # print("Generated response:", generated)
             results.append(generated)
     return results
 
 def main():
+    download_all_models()
     sentiment_estimator = sentiment.SentimentScaler()
-    tokenizer, model = models.load_model(models.mistral_model_name)
 
-    with open('mistral_result.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        writer = csv.writer(csvfile)
-        headers = ['sex', 'age', 'occupation', 'country', 'marital_status']
-        first_headers = []
-        third_headers = []
-        for i in range(1, 6):
-            first_headers += [f'first_q{i}', f'first_score{i}']
-            third_headers += [f'third_q{i}', f'third_score{i}']
-        for i in range(6,11):
-            first_headers += [f'first_q{i}']
-            third_headers += [f'third_q{i}']
-        headers += first_headers + third_headers
-        writer.writerow(headers)
+    for model_name in tqdm.tqdm([models.gemma_model_name, models.mistral_model_name, models.qwen_model_name]):
+        print(f"Loaded model: {model_name}")
+        tokenizer, model = models.load_model(models.qwen_model_name)
 
-        for persona in dataset.get_next_persona():
-            print('-' * 100)
-            result = cross_check_persona(persona, model, tokenizer, sentiment_estimator)
-            print('-' * 100)
-            writer.writerow(persona.tolist() + result)
+        with open(f'{model_name}_result.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            headers = ['sex', 'age', 'occupation', 'country', 'marital_status']
+            first_headers = []
+            third_headers = []
+            for i in range(1, 6):
+                first_headers += [f'first_q{i}', f'first_score{i}']
+                third_headers += [f'third_q{i}', f'third_score{i}']
+            for i in range(6,11):
+                first_headers += [f'first_q{i}']
+                third_headers += [f'third_q{i}']
+            headers += first_headers + third_headers
+            writer.writerow(headers)
+
+            for persona in tqdm.tqdm(dataset.get_next_persona(), total=dataset.get_length()):
+                # print('-' * 100)
+                result = cross_check_persona(persona, model, tokenizer, sentiment_estimator)
+                # print('-' * 100)
+                writer.writerow(persona.tolist() + result)
 
 if __name__ == "__main__":
     main()
